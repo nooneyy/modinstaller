fd8bf26e237ed84485a8c51740c67b2097829f58
Tomas Kolarik <tomaskolarik1@seznam.cz>
2023-04-20 19:47:04 +0200

    Change license to MIT, add future-proofing

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -4,7 +4,7 @@
 
 <div
   class="p-5 flex justify-center grow-0 dark:bg-zinc-800 dark:text-white transition-colors items-center space-x-3">
-  <p>Copyright © 2023 modinstaller Contributors. CC-BY / MIT</p>
+  <p>Copyright © {new Date().getFullYear()} modinstaller Contributors. MIT</p>
   <button
     class="mr-4 border-2 rounded-xl p-2"
     on:click={() => ($dark === "dark" ? dark.set("light") : dark.set("dark"))}>
